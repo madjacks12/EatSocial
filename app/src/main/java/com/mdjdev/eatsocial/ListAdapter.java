be8b93273a48db5cdef6477d3d365a84bc906fbe
MJ <mdjacksonvideo@gmail.com>
2018-03-16 11:21:18 -0700

    finish styling Mainactivity

---
+++ app/src/main/java/com/mdjdev/eatsocial/ListAdapter.java
@@ -0,0 +1,11 @@
+package com.mdjdev.eatsocial;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Matthew on 3/16/2018.
+ */
+
+public class ListAdapter extends ArrayAdapter {
+
+}
--- app/src/main/java/com/mdjdev/eatsocial/MainActivity.java
+++ app/src/main/java/com/mdjdev/eatsocial/MainActivity.java
@@ -1,12 +1,15 @@
 package com.mdjdev.eatsocial;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Typeface comfortaaFont = Typeface.createFromAsset(getAssets(), "fonts/Comfortaa_Regular.ttf");
+
+        mHeadingText.setTypeface(comfortaaFont);
+        mSubHeading.setTypeface(comfortaaFont);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (intent == null) {
+                    intent = new Intent(MainActivity.this, ListActivity.class);
+                }
+                startActivity(intent);
+            }
+        });
     }
-}
+
+    }
+
