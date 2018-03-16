aa8e3179d5ce626d90deadcddd92c186eba85e40
MJ <mdjacksonvideo@gmail.com>
2018-03-16 11:02:24 -0700

    add butterknife

--- app/src/main/java/com/mdjdev/eatsocial/MainActivity.java
+++ app/src/main/java/com/mdjdev/eatsocial/MainActivity.java
@@ -1,13 +1,24 @@
 package com.mdjdev.eatsocial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.headingText) TextView mHeadingText;
+    @Bind(R.id.subHeading) TextView mSubHeading;
+    @Bind(R.id.startButton) Button mStartButton;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
