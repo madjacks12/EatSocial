dcb6cddf9dc277ad2c924d2560dcacd367cc3885
MJ <mdjacksonvideo@gmail.com>
2018-03-16 11:50:20 -0700

    add list view activity with xml

--- app/src/main/java/com/mdjdev/eatsocial/ListActivity.java
+++ app/src/main/java/com/mdjdev/eatsocial/ListActivity.java
@@ -2,8 +2,20 @@ package com.mdjdev.eatsocial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.Bind;
 
 public class ListActivity extends AppCompatActivity {
+
+    @Bind(R.id.restaurantHeader) TextView mRestaurantHeader;
+    @Bind(R.id.listView) ListView mListView;
+
+    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
+    private String[] friends = new String[] {"Brice Phillips", "Susan Samson", "Chris Davis", "Linda Romano", "Thomas Shelby", "Ashford Delanto", "Britney Houston", "Scooter Richards", "Patty Daniels", "Phillip Buch", "Dave Johnson", "Kathy Smith", "Arnold California", "Del Olds", "Freddy Friday", "Lisa Simpson" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/mdjdev/eatsocial/ListAdapter.java
+++ app/src/main/java/com/mdjdev/eatsocial/ListAdapter.java
@@ -1,5 +1,6 @@
 package com.mdjdev.eatsocial;
 
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
 /**
@@ -8,4 +9,22 @@ import android.widget.ArrayAdapter;
 
 public class ListAdapter extends ArrayAdapter {
 
+    public ListAdapter(Context mContext, int resource, String[] mRestaurants, String[] mFriends) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.length;
+    }
 }
