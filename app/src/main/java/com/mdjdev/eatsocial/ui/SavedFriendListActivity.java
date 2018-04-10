7c5c3e7c6dd958d0aff33fa17e228d6c6dd4096e
Student <example@example.com>
2018-04-10 14:06:21 -0700

    refactor ListActivity

--- app/src/main/java/com/mdjdev/eatsocial/ui/SavedFriendListActivity.java
+++ app/src/main/java/com/mdjdev/eatsocial/ui/SavedFriendListActivity.java
@@ -20,8 +20,7 @@ public class SavedFriendListActivity extends AppCompatActivity {
     private DatabaseReference mRestaurantReference;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
 
-    @Bind(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
